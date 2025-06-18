7b3c1dc6ea6c82caae4664a776b22102cc4f1b9b
NileshMundotia <nileshmundotia@gmail.com>
2025-06-18 19:45:35 +0530

    Added trailer section component and updated footer and asset references

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -22,20 +22,20 @@ export const assets = {
 
 export const dummyTrailers = [
     {
-        image: "https://img.youtube.com/vi/WpW36ldAqnM/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=WpW36ldAqnM'
+        image: "https://img.youtube.com/vi/YH6k5weqwy8/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=YH6k5weqwy8'
     },
     {
-        image: "https://img.youtube.com/vi/-sAOWhvheK8/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=-sAOWhvheK8'
+        image: "https://img.youtube.com/vi/KymrMQKEZ2I/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=KymrMQKEZ2I'
     },
     {
-        image: "https://img.youtube.com/vi/1pHDWnXmK7Y/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=1pHDWnXmK7Y'
+        image: "https://img.youtube.com/vi/pAsmrKyMqaA/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=pAsmrKyMqaA'
     },
     {
-        image: "https://img.youtube.com/vi/umiKiW4En9g/maxresdefault.jpg",
-        videoUrl: 'https://www.youtube.com/watch?v=umiKiW4En9g'
+        image: "https://img.youtube.com/vi/uXr9Ha9qRHE/maxresdefault.jpg",
+        videoUrl: 'https://www.youtube.com/watch?v=uXr9Ha9qRHE'
     },
 ]
 
