49cbd20799f18efa5cf3bcd0fc374a846e2fa227
NileshMundotia <nileshmundotia@gmail.com>
2025-06-18 18:29:51 +0530

    Add TrailersSection component with ReactPlayer and update MovieCard and Home

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,6 +1,8 @@
 import { StarIcon } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import timeFormat from '../lib/timeFormat'
+// import { timeFormat } from '../lib/timeFormat'
 
 const MovieCard = ({movie}) => {
     const navigate=useNavigate()
@@ -8,7 +10,7 @@ const MovieCard = ({movie}) => {
     <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-64'>
         <img onClick={()=>{navigate(`/movies/${movie._id}`); scrollTo(0,0)}} src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer'/>
         <p className='font-semibold mt-2 truncate'>{movie.title}</p>
-        <p className='text-sm text-gray-400 mt-2'>{new Date(movie.release_date).getFullYear()}.{movie.genres.slice(0,2).map(genre=>genre.name).join("|")}.{movie.runtime}</p>
+        <p className='text-sm text-gray-400 mt-2'>{new Date(movie.release_date).getFullYear()} · {movie.genres.slice(0,2).map(genre=>genre.name).join(" | ")}  ·{timeFormat(movie.runtime)}</p>
         <div className='flex items-center justify-between mt-4 pb-3'>
            <button onClick={()=>{navigate(`/movies/${movie._id}`); scrollTo(0,0)}} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull
 transition rounded-full font-medium cursor-pointer'>Buy Tickets</button>
